05b3f97ac786ef0872eb352141d005b648da44c6
okondev2021 <sojapril16@gmail.com>
2025-01-01 21:52:13 +0100

    modified screens object in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'tab': {'max': '800px'},
+      // => @media (max-width: 1030px) { ... }
+      'mobile': {'max': '430px'},
+      // => @media (max-width: 430px) { ... }
+    },
+    extend: {
+      colors: {
+        darkBlue: "#252b46",
+        grayishBlue: "#9194a1",
+        softRed: "#fa5757",
+        customRed: "#c60e20",
+        softBlue: "#5368df",
+      }
+    },
   },
   plugins: [],
 }
