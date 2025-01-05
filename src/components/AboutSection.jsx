43677910a73d82c525aef00d96ff2d5cba208b79
David <davefast2003@gmail.com>
2025-01-05 15:33:57 +0100

    style: enhance AboutSection layout for better responsiveness

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const AboutSection = () => {
   return (
-    <section className="pagePadding my-20 flex flex-row gap-10 tab:flex-col">
+    <section id="about" className="pagePadding my-20 flex flex-row gap-10 tab:flex-col">
       <div className="h-full w-1/2 tab:w-full">
         <img
           src="https://plus.unsplash.com/premium_photo-1679547202606-4d905471107f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHNwZWFraW5nJTIwdG8lMjBhbiUyMGF1ZGllbmNlfGVufDB8fDB8fHww"
@@ -11,9 +11,9 @@ const AboutSection = () => {
           className="w-full"
         />
       </div>
-      <div className="flex w-1/2 flex-col justify-start px-16 tab:w-full">
+      <div className="flex w-1/2 flex-col justify-start px-16 tab:px-1 tab:w-full">
         <h1 className="mb-2 text-sm font-semibold text-customRed">About us</h1>
-        <h2 className="mb-6 text-3xl font-medium">
+        <h2 className="mb-6 text-3xl font-medium tab:text-2xl">
           We are trendsetters in Public Speaking.
         </h2>
         <p className="mb-8 text-sm text-gray-700">
